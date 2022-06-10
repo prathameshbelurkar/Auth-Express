094bc94206ee47a861b1808123125ec5c0a27559
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-06-10 12:20:16 +0530

    add router, accessing email and password

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -4,6 +4,22 @@ exports.router = void 0;
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 exports.router = router;
-router.get("/", (req, res) => {
-    res.send("hi there");
+router.get("/login", (req, res) => {
+    res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    res.send(email + password);
 });
